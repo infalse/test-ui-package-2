e5b0faa6ee913268f7019135de7ebfb0594619fa
Kirill Evtushenko <kirill.evtushenko@takeda.com>
2022-06-23 14:10:07 +0400

    fix: added exports of new components

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,7 @@ export { Divider } from './components/atoms/Divider';
 
 // molecules
 export { AnimatedHeavyMolecule } from './components/molecules/AnimatedHeavyMolecule';
+export { TakedaFeaturedArticle } from './components/molecules/TakedaFeaturedArticle';
+
+// organisms
+export { TakedaCarousel } from './components/organisms/TakedaCarousel';
